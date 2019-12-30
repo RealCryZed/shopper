f513150ab914f09c1e740cedfac28ae2987a4a21
RealCryZed <bujhmgtnhjd@bk.ru>
2019-12-30 15:45:48 +0600

    Now if you reach wrong url you will get an error page

--- src/main/java/shopper/Configs/WebMvcConfig.java
+++ src/main/java/shopper/Configs/WebMvcConfig.java
@@ -2,6 +2,7 @@ package shopper.Configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addStatusController("/error.html", HttpStatus.NOT_FOUND);
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
--- src/main/java/shopper/Controllers/ProductController.java
+++ src/main/java/shopper/Controllers/ProductController.java
@@ -181,12 +181,7 @@ public class ProductController {
     @GetMapping("/getProduct/{id}")
     public ModelAndView findProductById(@PathVariable Integer id) {
         ModelAndView modelAndView = new ModelAndView();
-
-        if (!productService.doesProductExistById(id)) {
-            modelAndView.setViewName("redirect:/error");
-            return modelAndView;
-        }
-
+        
         try {
             Product product = productService.getProductById(id);
             modelAndView.addObject("productById", product);
